bd2e3e3883201e4be36e63cb8d551c6c1a36a6c3
zainalalmasyhur <zainalalmasyhur30@gmail.com>
2024-06-13 17:28:57 +0700

    infinitescroll

--- server/models/comment.js
+++ server/models/comment.js
@@ -16,6 +16,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       comment: DataTypes.TEXT,
       UserId: DataTypes.INTEGER,
+      ChapterId: DataTypes.STRING,
     },
     {
       sequelize,
--- server/routers/index.js
+++ server/routers/index.js
@@ -3,11 +3,27 @@ const router = express.Router();
 
 const LoginController = require(`../controllers/LoginController`);
 const RegisterController = require(`../controllers/RegisterController`);
+const CommentController = require(`../controllers/CommentController`);
 const { errorHandler } = require(`../middleware/errorhandler`);
+
+const {
+  authentication,
+  authorization,
+} = require(`../middleware/authentication`);
 
 router.post("/login", LoginController.login);
 router.post("/login-google", LoginController.loginByGoogle);
 router.post("/register", RegisterController.Register);
+
+router.use(authentication);
+router.get("/comments/:chapterId", CommentController.getComment);
+router.post("/comments", CommentController.postComment);
+router.put("/comments/:id", authorization, CommentController.updateCommentById);
+router.delete(
+  "/comments/:id",
+  authorization,
+  CommentController.deleteCommentById
+);
 
 router.use(errorHandler);
 
